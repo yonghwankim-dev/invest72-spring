5d7575333ff1ee6caab9d93133b198b485a8aca2
yonghwankim-dev <dragonbead95@naver.com>
2026-02-23 16:44:00 +0900

    add null source

--- src/test/java/source/ProductAmountTestDataProvider.java
+++ src/test/java/source/ProductAmountTestDataProvider.java
@@ -17,6 +17,7 @@ public class ProductAmountTestDataProvider {
 
 	public static Stream<Arguments> invalidAmounts() {
 		return Stream.of(
+			Arguments.of((java.math.BigDecimal)null),
 			Arguments.of(new java.math.BigDecimal("-0.01")),
 			Arguments.of(new java.math.BigDecimal("-1")),
 			Arguments.of(new java.math.BigDecimal("10000000000000.01")),
