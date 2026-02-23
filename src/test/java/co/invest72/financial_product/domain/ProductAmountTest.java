862f8f32b12c0b8ec7aa1e1f9a7e58601c546179
yonghwankim-dev <dragonbead95@naver.com>
2026-02-23 16:43:07 +0900

    add edge case - ProductAmount

--- src/test/java/co/invest72/financial_product/domain/ProductAmountTest.java
+++ src/test/java/co/invest72/financial_product/domain/ProductAmountTest.java
@@ -4,22 +4,29 @@ import java.math.BigDecimal;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class ProductAmountTest {
 
 	private void assertDoesNotThrowAnyExceptionForAmount(BigDecimal value) {
+
+	}
+
+	@DisplayName("금액이 범위 안에 있는 경우 인스턴스가 생성되어야 한다.")
+	@ParameterizedTest(name = "금액: {0}")
+	@MethodSource(value = "source.ProductAmountTestDataProvider#validAmounts")
+	void newInstance_whenAmountIsValid_thenCreateInstance(BigDecimal value) {
 		Assertions.assertThatCode(() -> new ProductAmount(value))
 			.doesNotThrowAnyException();
 	}
 
-	@DisplayName("금액이 0원 이상이어야 한다.")
-	@Test
-	void newInstance_whenAmountIsValid_thenCreateInstance() {
-		assertDoesNotThrowAnyExceptionForAmount(BigDecimal.ZERO);
-		assertDoesNotThrowAnyExceptionForAmount(new BigDecimal("0.01"));
-		assertDoesNotThrowAnyExceptionForAmount(new BigDecimal("1000"));
-		assertDoesNotThrowAnyExceptionForAmount(new BigDecimal("9999999999999.99"));
-		assertDoesNotThrowAnyExceptionForAmount(new BigDecimal("10000000000000"));
+	@DisplayName("금액이 범위를 벗어난 경우 예외가 발생해야 한다.")
+	@ParameterizedTest(name = "금액: {0}")
+	@MethodSource(value = "source.ProductAmountTestDataProvider#invalidAmounts")
+	void newInstance_whenAmountIsInvalid_thenThrowException(BigDecimal value) {
+		Assertions.assertThatThrownBy(() -> new ProductAmount(value))
+			.isInstanceOf(IllegalArgumentException.class);
 	}
+
 }
