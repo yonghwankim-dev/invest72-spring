9fe9615ec6fc949a24b7fa02e2b2011ed13ba60e
yonghwankim-dev <dragonbead95@naver.com>
2026-02-24 10:42:05 +0900

    fix message with DecimalMax

--- src/main/java/co/invest72/common/validation/FinancialRate.java
+++ src/main/java/co/invest72/common/validation/FinancialRate.java
@@ -17,7 +17,7 @@ import jakarta.validation.constraints.NotNull;
 @Constraint(validatedBy = {})
 @NotNull(message = "비율 설정은 필수입니다.")
 @DecimalMin(value = "0", message = "비율은 0% 이상이어야 합니다.")
-@DecimalMax(value = "9.9999", message = "비율은 최대 999.99%까지 설정 가능합니다.")
+@DecimalMax(value = "9.9999", message = "비율은 최대 9.9999%까지 설정 가능합니다.")
 @Documented
 public @interface FinancialRate {
 	String message() default "유효하지 않은 비율입니다.";
