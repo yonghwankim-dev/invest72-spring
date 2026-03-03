9ad4acbe8fc59a435e1821ac40fbd615cf38ba7b
yonghwankim-dev <dragonbead95@naver.com>
2026-03-03 16:53:14 +0900

    feat - add @Param annotation to findByUserId method in JpaFinancialProductRepository

--- src/main/java/co/invest72/financial_product/infrastructure/repository/JpaFinancialProductRepository.java
+++ src/main/java/co/invest72/financial_product/infrastructure/repository/JpaFinancialProductRepository.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import co.invest72.financial_product.domain.FinancialProduct;
 
 public interface JpaFinancialProductRepository extends JpaRepository<FinancialProduct, String> {
 
 	@Query("SELECT p FROM FinancialProduct p WHERE p.userId = :userId")
-	List<FinancialProduct> findByUserId(String userId);
+	List<FinancialProduct> findByUserId(@Param("userId") String userId);
 }
