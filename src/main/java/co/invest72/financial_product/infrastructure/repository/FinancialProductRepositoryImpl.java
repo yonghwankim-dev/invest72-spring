c64b83bb5bcd1bb1d216c9530e56128d59c0d787
YongHwan Kim <dragonbead95@naver.com>
2026-03-03 22:03:15 +0900

    [feat] DB 연동 구현 (#86)
    * feat - implement FinancialProductRepositoryImpl with PostgreSQL database
    
    * feat - add JpaUserRepository and UserRepositoryImpl for user management
    
    * feat - update PostgreSQL version and improve docker-compose configuration
    
    * feat - refactor database configuration to use environment variables and update docker-compose
    
    * feat - update database configuration for PostgreSQL with environment variables and add production profile
    
    * feat - add local and production docker-compose configurations with environment files
    
    * feat - add findByProviderId method to JpaUserRepository and update UserRepositoryImpl to use it
    
    * feat - extend local and production docker-compose configurations to include shared services
    
    * feat - update application.yaml to set default PostgreSQL username and password
    
    * feat - add @Param annotation to findByUserId method in JpaFinancialProductRepository
    
    * feat - update PostgreSQL image version to 17.0 in docker-compose.yaml
    
    * feat - update PostgreSQL image to 17.0-alpine in docker-compose.yaml
    
    * feat - update findByProductId method to return null if product is not found
    
    * feat - configure test profiles and add H2 database for testing
    
    * fix - change db container service volume path
    
    * docs - add Docker Compose usage instructions to README.md

---
+++ src/main/java/co/invest72/financial_product/infrastructure/repository/FinancialProductRepositoryImpl.java
@@ -0,0 +1,52 @@
+package co.invest72.financial_product.infrastructure.repository;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+import co.invest72.financial_product.domain.FinancialProduct;
+import co.invest72.financial_product.domain.FinancialProductRepository;
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+@Primary
+@Profile("!test")
+public class FinancialProductRepositoryImpl implements FinancialProductRepository {
+
+	private final JpaFinancialProductRepository jpaRepository;
+
+	@Override
+	public String save(FinancialProduct product) {
+		return jpaRepository.save(product).getId();
+	}
+
+	/**
+	 * ID로 금융 상품을 조회합니다.
+	 * ID로 조회된 금융 상품이 존재하지 않으면 null을 반환합니다.
+	 * @param id 조회할 금융 상품의 ID
+	 * @return 조회된 금융 상품, 존재하지 않으면 null
+	 */
+	@Override
+	public FinancialProduct findByProductId(String id) {
+		return jpaRepository.findById(id)
+			.orElse(null);
+	}
+
+	@Override
+	public List<FinancialProduct> findAllByUserId(String userId) {
+		return jpaRepository.findByUserId(userId);
+	}
+
+	@Override
+	public void deleteByProductId(String productId) {
+		jpaRepository.deleteById(productId);
+	}
+
+	@Override
+	public void clear() {
+		jpaRepository.deleteAll();
+	}
+}
--- src/test/java/co/invest72/financial_product/presentation/FinancialProductCalculationRestControllerTest.java
+++ src/test/java/co/invest72/financial_product/presentation/FinancialProductCalculationRestControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
@@ -35,6 +36,7 @@ import source.FinancialProductDataProvider;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class FinancialProductCalculationRestControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
