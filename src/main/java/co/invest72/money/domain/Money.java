0f80cd43112c3726704a632ba9a7234b003b6fdc
yonghwankim-dev <dragonbead95@naver.com>
2026-03-12 11:33:37 +0900

    feat - implement add method in Money

--- src/main/java/co/invest72/money/domain/Money.java
+++ src/main/java/co/invest72/money/domain/Money.java
@@ -6,13 +6,30 @@ import java.util.Objects;
 public class Money {
 
 	private final BigDecimal value;
+	private final String currency;
 
-	private Money(BigDecimal value) {
+	private Money(BigDecimal value, String currency) {
 		this.value = value;
+		this.currency = currency;
 	}
 
 	public static Money dollar(int value) {
-		return new Money(BigDecimal.valueOf(value));
+		return new Money(BigDecimal.valueOf(value), "USD");
+	}
+
+	public static Money won(int value) {
+		return new Money(BigDecimal.valueOf(value), "KRW");
+	}
+
+	public Money add(Money addend) {
+		validate(addend);
+		return new Money(this.value.add(addend.value), this.currency);
+	}
+
+	private void validate(Money money) {
+		if (!this.currency.equalsIgnoreCase(money.currency)) {
+			throw new IllegalArgumentException("통화가 일치하지 않습니다.");
+		}
 	}
 
 	@Override
@@ -22,11 +39,11 @@ public class Money {
 		if (o == null || getClass() != o.getClass())
 			return false;
 		Money money = (Money)o;
-		return this.value.compareTo(money.value) == 0;
+		return this.value.compareTo(money.value) == 0 && this.currency.equalsIgnoreCase(money.currency);
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(value);
+		return Objects.hash(value, currency);
 	}
 }
--- src/test/java/co/invest72/money/domain/MoneyTest.java
+++ src/test/java/co/invest72/money/domain/MoneyTest.java
@@ -21,11 +21,42 @@ class MoneyTest {
 		// given
 		Money money1 = Money.dollar(5);
 		Money money2 = Money.dollar(5);
+		// when & then
+		Assertions.assertThat(money1)
+			.hasSameHashCodeAs(money2)
+			.isEqualTo(money2);
+	}
+
+	@DisplayName("객체 생성 - 원화 생성")
+	@Test
+	void newInstance_whenCurrentIsKRW_thenReturnMoney() {
 		// when
-		boolean result = money1.equals(money2);
+		Money money = Money.won(1000);
 		// then
-		Assertions.assertThat(result).isTrue();
-		Assertions.assertThat(money1).hasSameHashCodeAs(money2);
-		Assertions.assertThat(money1).isEqualTo(money2);
+		Assertions.assertThat(money).isNotNull();
+	}
+
+	@DisplayName("금액 비교 - 원화와 달러를 비교했을때 달라야 한다.")
+	@Test
+	void equals_whenSameValueButDifferentCurrency_thenReturnFalse() {
+		// given
+		Money money1 = Money.dollar(5);
+		Money money2 = Money.won(5);
+		// when & then
+		Assertions.assertThat(money1)
+			.doesNotHaveSameHashCodeAs(money2)
+			.isNotEqualTo(money2);
+	}
+
+	@DisplayName("달러 덧셈 - 두개의 달러를 더했을 때, 새로운 달러 객체가 반환되어야 한다.")
+	@Test
+	void add_whenMoneyIsDollar_thenReturnNewSumDollar() {
+		// given
+		Money fiveBucks = Money.dollar(5);
+		Money tenBucks = Money.dollar(10);
+		// when
+		Money result = fiveBucks.add(tenBucks);
+		// then
+		Assertions.assertThat(result).isEqualTo(Money.dollar(15));
 	}
 }
