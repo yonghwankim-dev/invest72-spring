56aeff15f7068b02cc99f91c98bfc7109ae3f1d7
yonghwankim-dev <dragonbead95@naver.com>
2026-03-12 16:59:10 +0900

    refactor - rename field, moneyAmount to amount

--- src/main/java/co/invest72/investment/domain/amount/YearlyInstallmentInvestmentAmount.java
+++ src/main/java/co/invest72/investment/domain/amount/YearlyInstallmentInvestmentAmount.java
@@ -8,32 +8,32 @@ import co.invest72.investment.domain.InterestRate;
 import co.invest72.money.domain.Money;
 
 public class YearlyInstallmentInvestmentAmount implements InstallmentInvestmentAmount {
-	private final Money moneyAmount;
+	private final Money amount;
 
-	public YearlyInstallmentInvestmentAmount(Money moneyAmount) {
-		this.moneyAmount = moneyAmount;
-		if (this.moneyAmount.getValue().compareTo(BigDecimal.ZERO) < 0) {
+	public YearlyInstallmentInvestmentAmount(Money amount) {
+		this.amount = amount;
+		if (this.amount.getValue().compareTo(BigDecimal.ZERO) < 0) {
 			throw new IllegalArgumentException("금액은 음수일 수 없습니다.");
 		}
 	}
 
 	@Override
 	public BigDecimal getMonthlyAmount() {
-		return this.moneyAmount.getValue().divide(BigDecimal.valueOf(12), RoundingMode.HALF_EVEN);
+		return this.amount.getValue().divide(BigDecimal.valueOf(12), RoundingMode.HALF_EVEN);
 	}
 
 	@Override
 	public BigDecimal calAnnualInterest(InterestRate interestRate) {
-		return interestRate.getAnnualInterest(moneyAmount.getValue());
+		return interestRate.getAnnualInterest(amount.getValue());
 	}
 
 	@Override
 	public BigDecimal calMonthlyInterest(InterestRate interestRate) {
-		return interestRate.calMonthlyInterest(moneyAmount.getValue());
+		return interestRate.calMonthlyInterest(amount.getValue());
 	}
 
 	@Override
 	public BigDecimal getAmount() {
-		return moneyAmount.getValue();
+		return amount.getValue();
 	}
 }
