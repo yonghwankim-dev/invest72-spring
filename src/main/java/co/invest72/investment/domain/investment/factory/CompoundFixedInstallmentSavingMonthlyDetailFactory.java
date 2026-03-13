78d1c12f42e5e34c3535da20eab16e0e20e506a0
yonghwankim-dev <dragonbead95@naver.com>
2026-03-13 15:12:42 +0900

    feat - update code, createDetails, BigDecimal to Money

--- src/main/java/co/invest72/investment/domain/investment/factory/CompoundFixedInstallmentSavingMonthlyDetailFactory.java
+++ src/main/java/co/invest72/investment/domain/investment/factory/CompoundFixedInstallmentSavingMonthlyDetailFactory.java
@@ -8,6 +8,7 @@ import co.invest72.investment.domain.InterestRate;
 import co.invest72.investment.domain.InvestPeriod;
 import co.invest72.investment.domain.InvestmentAmount;
 import co.invest72.investment.domain.investment.MonthlyInvestmentDetail;
+import co.invest72.money.domain.Money;
 
 public class CompoundFixedInstallmentSavingMonthlyDetailFactory {
 
@@ -25,16 +26,17 @@ public class CompoundFixedInstallmentSavingMonthlyDetailFactory {
 
 	public List<MonthlyInvestmentDetail> createDetails() {
 		List<MonthlyInvestmentDetail> result = new ArrayList<>();
-		BigDecimal principal = BigDecimal.ZERO;
-		BigDecimal interest = BigDecimal.ZERO;
-		BigDecimal profit = BigDecimal.ZERO;
+		// todo: 원화가 아닌 통화도 지원하도록 수정 필요
+		Money principal = Money.won(BigDecimal.ZERO);
+		Money interest = Money.won(BigDecimal.ZERO);
+		Money profit = Money.won(BigDecimal.ZERO);
 
-		result.add(new MonthlyInvestmentDetail(0, principal, interest, profit));
+		result.add(new MonthlyInvestmentDetail(0, principal.getValue(), interest.getValue(), profit.getValue()));
 		for (int i = 1; i <= investPeriod.getMonths(); i++) {
-			principal = profit.add(investmentAmount.getAmount().getValue());
-			interest = interestRate.getMonthlyRate().multiply(principal);
+			principal = profit.add(investmentAmount.getAmount());
+			interest = interestRate.calMonthlyInterest(principal);
 			profit = principal.add(interest);
-			result.add(new MonthlyInvestmentDetail(i, principal, interest, profit));
+			result.add(new MonthlyInvestmentDetail(i, principal.getValue(), interest.getValue(), profit.getValue()));
 		}
 		return result;
 	}
