6b9babcc51bae3ac4c8a699d0c9129ceffecf483
yonghwankim-dev <dragonbead95@naver.com>
2026-03-13 14:59:28 +0900

    feat - update code, createDetails, change type BigDeciaml to Money

--- src/main/java/co/invest72/investment/domain/investment/factory/CompoundFixedDepositMonthlyDetailFactory.java
+++ src/main/java/co/invest72/investment/domain/investment/factory/CompoundFixedDepositMonthlyDetailFactory.java
@@ -8,6 +8,7 @@ import co.invest72.investment.domain.InterestRate;
 import co.invest72.investment.domain.InvestPeriod;
 import co.invest72.investment.domain.InvestmentAmount;
 import co.invest72.investment.domain.investment.MonthlyInvestmentDetail;
+import co.invest72.money.domain.Money;
 
 public class CompoundFixedDepositMonthlyDetailFactory {
 
@@ -25,16 +26,17 @@ public class CompoundFixedDepositMonthlyDetailFactory {
 	public List<MonthlyInvestmentDetail> createDetails() {
 		List<MonthlyInvestmentDetail> result = new ArrayList<>();
 
-		BigDecimal principal = investmentAmount.getAmount().getValue();
-		BigDecimal interest = BigDecimal.ZERO;
-		BigDecimal profit = investmentAmount.getAmount().getValue();
-		result.add(new MonthlyInvestmentDetail(0, principal, interest, profit));
+		Money principal = investmentAmount.getAmount();
+		// TODO: 원화가 아닌 통화도 지원하도록 수정 필요
+		Money interest = Money.won(BigDecimal.ZERO);
+		Money profit = investmentAmount.getAmount();
+		result.add(new MonthlyInvestmentDetail(0, principal.getValue(), interest.getValue(), profit.getValue()));
 
 		for (int i = 1; i <= investPeriod.getMonths(); i++) {
 			principal = profit;
-			interest = interestRate.getMonthlyRate().multiply(principal);
+			interest = interestRate.calMonthlyInterest(principal);
 			profit = principal.add(interest);
-			result.add(new MonthlyInvestmentDetail(i, principal, interest, profit));
+			result.add(new MonthlyInvestmentDetail(i, principal.getValue(), interest.getValue(), profit.getValue()));
 		}
 		return result;
 	}
