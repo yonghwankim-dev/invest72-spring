404fd5014079887750b7babb4cc31cff30fff308
yonghwankim-dev <dragonbead95@naver.com>
2026-03-23 15:06:12 +0900

    style - formatted code

--- src/main/java/co/invest72/financial_product/domain/service/FinancialProductCalculator.java
+++ src/main/java/co/invest72/financial_product/domain/service/FinancialProductCalculator.java
@@ -12,25 +12,28 @@ public class FinancialProductCalculator {
 		InvestmentType investmentType = InvestmentType.valueOf(product.getInvestmentTypeName());
 		LocalDate startDate = product.getStartDate();
 		Integer months = product.getMonthsValue();
+
 		return investmentType.calculateExpirationDate(startDate, months);
 	}
 
 	public BigDecimal calculateBalance(FinancialProduct product, LocalDate today) {
 		InvestmentType investmentType = InvestmentType.valueOf(product.getInvestmentTypeName());
+
 		return investmentType.calculateBalance(product, today);
 	}
 
 	public BigDecimal calculateProgress(FinancialProduct product, LocalDate today) {
 		InvestmentType investmentType = InvestmentType.valueOf(product.getInvestmentTypeName());
-
 		LocalDate startDate = product.getStartDate();
 		LocalDate expirationDate = calculateExpirationDate(product);
+
 		return investmentType.calculateProgress(startDate, expirationDate, today);
 	}
 
 	public Long calculateRemainingDays(FinancialProduct product, LocalDate today) {
 		InvestmentType investmentType = InvestmentType.valueOf(product.getInvestmentTypeName());
 		LocalDate expirationDate = calculateExpirationDate(product);
+
 		return investmentType.calculateRemainingDays(today, expirationDate);
 	}
 }
