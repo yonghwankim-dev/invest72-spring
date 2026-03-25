51fdec6543bb40402cf9ef325e63ede58a11a408
yonghwankim-dev <dragonbead95@naver.com>
2026-03-25 15:26:42 +0900

    test - change to FinancialProductCalculator

--- src/test/java/co/invest72/investment/domain/investment/BalancePolicyTest.java
+++ src/test/java/co/invest72/investment/domain/investment/BalancePolicyTest.java
@@ -4,13 +4,22 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import co.invest72.financial_product.domain.FinancialProduct;
+import co.invest72.financial_product.domain.service.FinancialProductCalculator;
 import source.FinancialProductDataProvider;
 
 class BalancePolicyTest {
+
+	private FinancialProductCalculator calculator;
+
+	@BeforeEach
+	void setUp() {
+		calculator = new FinancialProductCalculator();
+	}
 
 	@DisplayName("투자 금액 계산 - FIXED, 기준일자 상관없이 원금을 반환한다")
 	@Test
@@ -19,7 +28,7 @@ class BalancePolicyTest {
 		BalancePolicy policy = BalancePolicy.FIXED;
 		FinancialProduct product = FinancialProductDataProvider.createDepositProduct("user-1");
 		LocalDate today = LocalDate.of(2026, 1, 1);
-		LocalDate expirationDate = product.getExpirationDate();
+		LocalDate expirationDate = calculator.calculateExpirationDate(product);
 
 		// When
 		BigDecimal balance = policy.calculate(product, today, expirationDate);
@@ -36,7 +45,7 @@ class BalancePolicyTest {
 		BalancePolicy policy = BalancePolicy.ACCUMULATIVE;
 		FinancialProduct product = FinancialProductDataProvider.createSavingsProduct("user-1");
 		LocalDate today = LocalDate.of(2025, 12, 31);
-		LocalDate expirationDate = product.getExpirationDate();
+		LocalDate expirationDate = calculator.calculateExpirationDate(product);
 
 		// When
 		BigDecimal balance = policy.calculate(product, today, expirationDate);
@@ -53,7 +62,7 @@ class BalancePolicyTest {
 		BalancePolicy policy = BalancePolicy.ACCUMULATIVE;
 		FinancialProduct product = FinancialProductDataProvider.createSavingsProduct("user-1");
 		LocalDate today = LocalDate.of(2027, 1, 1);
-		LocalDate expirationDate = product.getExpirationDate();
+		LocalDate expirationDate = calculator.calculateExpirationDate(product);
 
 		// When
 		BigDecimal balance = policy.calculate(product, today, expirationDate);
