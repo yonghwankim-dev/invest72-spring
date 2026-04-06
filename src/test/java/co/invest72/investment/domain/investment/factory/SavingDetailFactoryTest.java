c4d0b64001ce5349362a6ce8297c69495493f081
yonghwankim-dev <dragonbead95@naver.com>
2026-04-06 13:40:15 +0900

    test - add test

--- src/test/java/co/invest72/investment/domain/investment/factory/SavingDetailFactoryTest.java
+++ src/test/java/co/invest72/investment/domain/investment/factory/SavingDetailFactoryTest.java
@@ -121,6 +121,30 @@ class SavingDetailFactoryTest {
 		Assertions.assertThat(details.get(2).getProfit()).isEqualTo(Money.won(BigDecimal.valueOf(13_379_166.67)));
 	}
 
+	@DisplayName("단리-적금-년도별 데이터 생성 - 통화가 USD인 경우")
+	@Test
+	void givenFactory_whenSimpleYearlyUSD_thenReturnDetails() {
+		// given
+		factory = ((SavingDetailFactory)factory).toBuilder()
+			.investmentAmount(new MonthlyInstallmentInvestmentAmount(Money.dollar(1_000_000)))
+			.investPeriod(new MonthlyInvestPeriod(13))
+			.build();
+		// When
+		List<InvestmentDetail> details = factory.createYearlyDetails();
+
+		// Then
+		Assertions.assertThat(details).hasSize(3);
+		Assertions.assertThat(details.get(0).getPrincipal()).isEqualTo(Money.dollar(0));
+		Assertions.assertThat(details.get(0).getInterest()).isEqualTo(Money.dollar(0));
+		Assertions.assertThat(details.get(0).getProfit()).isEqualTo(Money.dollar(0));
+		Assertions.assertThat(details.get(1).getPrincipal()).isEqualTo(Money.dollar(BigDecimal.valueOf(12_000_000)));
+		Assertions.assertThat(details.get(1).getInterest()).isEqualTo(Money.dollar(BigDecimal.valueOf(325_000)));
+		Assertions.assertThat(details.get(1).getProfit()).isEqualTo(Money.dollar(BigDecimal.valueOf(12_325_000)));
+		Assertions.assertThat(details.get(2).getPrincipal()).isEqualTo(Money.dollar(BigDecimal.valueOf(13_325_000)));
+		Assertions.assertThat(details.get(2).getInterest()).isEqualTo(Money.dollar(BigDecimal.valueOf(54_166.67)));
+		Assertions.assertThat(details.get(2).getProfit()).isEqualTo(Money.dollar(BigDecimal.valueOf(13_379_166.67)));
+	}
+
 	@DisplayName("복리-적금-년도별 데이터 생성 - 통화가 KRW인 경우")
 	@Test
 	void givenFactory_whenCompoundYearlyKRW_thenReturnDetails() {
