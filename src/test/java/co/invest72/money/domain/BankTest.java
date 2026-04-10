b623bfac95129e8583298621ca73e9f4336429b1
yonghwankim-dev <dragonbead95@naver.com>
2026-04-10 14:44:12 +0900

    test - introduce field

--- src/test/java/co/invest72/money/domain/BankTest.java
+++ src/test/java/co/invest72/money/domain/BankTest.java
@@ -1,25 +1,23 @@
 package co.invest72.money.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BankTest {
 
-	@DisplayName("객체 생성")
-	@Test
-	void canCreated() {
-		// when
-		Bank bank = new Bank();
-		// then
-		Assertions.assertThat(bank).isNotNull();
-	}
+	private Bank bank;
 
+	@BeforeEach
+	void setUp() {
+		bank = new Bank();
+	}
+	
 	@DisplayName("환전 - 원화를 달러로 환전한다")
 	@Test
 	void reduce_givenWonMoney_whenTargetIsDollar_thenReturnDollarMoney() {
 		// given
-		Bank bank = new Bank();
 		Money wonMoney = Money.won(1000);
 		Currency dollarCurrency = Currency.dollar();
 		// when
@@ -33,7 +31,6 @@ class BankTest {
 	@Test
 	void reduce_whenSourceIsDollar_thenReturnWonMoney() {
 		// given
-		Bank bank = new Bank();
 		Money oneBucks = Money.dollar(1);
 		Currency wonCurrency = Currency.won();
 		// when
