e2b9793b324380773f2854df84b56de29c9c09e2
yonghwankim-dev <dragonbead95@naver.com>
2026-04-14 11:28:12 +0900

    feat - imple convert String to BigDecimal in ExhcnageJsonResponse

--- src/main/java/co/invest72/exchange_rate/infrastructure/api/ExchangeJsonResponse.java
+++ src/main/java/co/invest72/exchange_rate/infrastructure/api/ExchangeJsonResponse.java
@@ -1,5 +1,6 @@
 package co.invest72.exchange_rate.infrastructure.api;
 
+import java.math.BigDecimal;
 import java.util.Objects;
 
 import lombok.Getter;
@@ -8,12 +9,12 @@ import lombok.Getter;
 public class ExchangeJsonResponse {
 	private final Integer result; // 결과
 	private final String currencyUnit; // 통화코드
-	private final String dealingBaseRate; // 매매기준율
+	private final BigDecimal dealingBaseRate; // 매매기준율
 
 	public ExchangeJsonResponse(Integer result, String currencyUnit, String dealingBaseRate) {
 		this.result = Objects.requireNonNull(result);
 		this.currencyUnit = Objects.requireNonNull(currencyUnit);
-		this.dealingBaseRate = Objects.requireNonNull(dealingBaseRate);
+		this.dealingBaseRate = new BigDecimal(dealingBaseRate.replace(",", ""));
 	}
 
 	@Override
