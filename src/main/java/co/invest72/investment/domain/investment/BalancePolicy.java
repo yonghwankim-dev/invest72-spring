028af496b69734082005bec58840aa8a73b0cdfe
yonghwankim-dev <dragonbead95@naver.com>
2026-05-08 15:55:41 +0900

    feat - update BalancePoliy accumurative logic
    - 적금 상품 가입시 초회납입 개념 적용
    - 상품 가입월의 정기납입은 건너뜀

--- src/main/java/co/invest72/investment/domain/investment/BalancePolicy.java
+++ src/main/java/co/invest72/investment/domain/investment/BalancePolicy.java
@@ -5,8 +5,6 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 import co.invest72.financial_product.domain.FinancialProduct;
-import co.invest72.financial_product.domain.ProductAmount;
-import co.invest72.financial_product.domain.ProductMonths;
 import co.invest72.money.domain.Currency;
 import co.invest72.money.domain.Money;
 
@@ -26,22 +24,33 @@ public enum BalancePolicy implements BalanceStrategy {
 		public Money calculate(FinancialProduct product, Currency productCurrency, LocalDate today,
 			LocalDate expirationDate) {
 			LocalDate startDate = product.getStartDate();
-			ProductAmount amount = product.getAmount();
-			ProductMonths months = product.getMonths();
 
 			if (today.isBefore(startDate)) {
 				return Money.of(BigDecimal.ZERO, productCurrency);
 			}
-			if (today.isAfter(expirationDate)) {
-				Money money = Money.of(amount.getValue(), productCurrency);
-				return money.times(BigDecimal.valueOf(months.getValue()));
+
+			// 초회납입
+			long count = 1;
+			// 정기납입 계산
+			// 예: 가입 1월 -> 계산 시작점 2월 1일
+			LocalDate regularPaymentStandardDate = startDate.plusMonths(1).withDayOfMonth(1);
+
+			if (!today.isBefore(regularPaymentStandardDate)) {
+				long monthsSinceNextMonth = ChronoUnit.MONTHS.between(regularPaymentStandardDate,
+					today.withDayOfMonth(1));
+				count += monthsSinceNextMonth;
+
+				// 오늘이 이번 달 납입 이후라면 1회 추가
+				if (product.isPaidOn(today)) {
+					count++;
+				}
 			}
-			long elapsedMonths = ChronoUnit.MONTHS.between(startDate, today);
-			if (product.isPaidOn(today)) {
-				elapsedMonths++;
-			}
-			Money money = Money.of(amount.getValue(), productCurrency);
-			return money.times(BigDecimal.valueOf(elapsedMonths));
+
+			// 약정된 총 횟수(예: 12회)를 넘지 않도록 제한
+			long finalCount = Math.min(count, product.getMonths().getValue());
+
+			Money money = Money.of(product.getAmount().getValue(), productCurrency);
+			return money.times(BigDecimal.valueOf(finalCount));
 		}
 	}
 }
