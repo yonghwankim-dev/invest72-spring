6ca7c6b79d63fb2c16348731267a8f17664dbe42
yonghwankim-dev <dragonbead95@naver.com>
2026-05-11 12:40:27 +0900

    test - add test, protocol edge case

--- src/test/java/co/invest72/security/AuthorizedRedirectUriCheckerTest.java
+++ src/test/java/co/invest72/security/AuthorizedRedirectUriCheckerTest.java
@@ -27,4 +27,26 @@ class AuthorizedRedirectUriCheckerTest {
 		// then
 		Assertions.assertThat(actual).isTrue();
 	}
+
+	@DisplayName("URI 검사 - 출처가 프로토콜이 다르면 false를 반환하여야 한다")
+	@Test
+	void check_whenProtocolIsDiff_thenReturnFalse() {
+		// given
+		String uri = "https://localhost:3000";
+		// when
+		boolean actual = checker.check(uri);
+		// then
+		Assertions.assertThat(actual).isFalse();
+	}
+
+	@DisplayName("URI 검사 - 허용되지 않는 도메인은 false를 반환하여야 한다")
+	@Test
+	void check_whenUriIsNotAllowedOrigin_thenReturnFalse() {
+		// given
+		String uri = "http://hacker.com";
+		// when
+		boolean actual = checker.check(uri);
+		// then
+		Assertions.assertThat(actual).isFalse();
+	}
 }
