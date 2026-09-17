583929f9db29595e5153135502f8eda0638fc98d
yonghwankim-dev <dragonbead95@naver.com>
2026-09-17 13:27:17 +0900

    test - add edge case test

--- src/test/java/co/invest72/investment/domain/DailyInvestPeriodTest.java
+++ src/test/java/co/invest72/investment/domain/DailyInvestPeriodTest.java
@@ -6,6 +6,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("일일 투자 기간 단위 테스트")
 class DailyInvestPeriodTest {
@@ -19,11 +21,22 @@ class DailyInvestPeriodTest {
 			// given
 			LocalDate startDate = null;
 			int days = 30;
-			// when
+			// when & then
 			Assertions.assertThatThrownBy(() -> new DailyInvestPeriod(startDate, days))
 				.isInstanceOf(IllegalArgumentException.class)
 				.hasMessage("startDate must not null");
 		}
 
+		@ParameterizedTest
+		@ValueSource(ints = {-1, -10, -100})
+		@DisplayName("days가 음수이면 IllegalArgumentException 예외가 발생한다")
+		void should_throw_exception_when_days_is_negative(int negativeDays) {
+			// given
+			LocalDate startDate = LocalDate.of(2026, 9, 11);
+			// when & then
+			Assertions.assertThatThrownBy(() -> new DailyInvestPeriod(startDate, negativeDays))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("days must not negative, days=" + negativeDays);
+		}
 	}
 }
