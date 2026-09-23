30ad9f6af3060b518b780175929dd0e3c8f5f8bf
yonghwankim-dev <dragonbead95@naver.com>
2026-09-23 11:46:06 +0900

    test - fix ExchangeRateUpdateHnadler

--- src/test/java/co/invest72/exchange_rate/application/ExchangeRateUpdateHandlerTest.java
+++ src/test/java/co/invest72/exchange_rate/application/ExchangeRateUpdateHandlerTest.java
@@ -4,14 +4,11 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
-import org.mockito.BDDMockito;
-import org.mockito.Mockito;
 
 import co.invest72.exchange_rate.domain.ExchangeRateRepository;
-import co.invest72.exchange_rate.domain.entity.ExchangeRate;
 import co.invest72.exchange_rate.domain.service.ExchangeRateService;
 import co.invest72.exchange_rate.infrastructure.api.ExchangeJsonResponse;
 import co.invest72.exchange_rate.infrastructure.persistence.InMemoryExchangeRateRepository;
@@ -20,12 +17,18 @@ import co.invest72.money.domain.CurrencyPair;
 
 class ExchangeRateUpdateHandlerTest {
 
+	private ExchangeRateService service;
+
+	@BeforeEach
+	void setUp() {
+		ExchangeRateRepository repository = new InMemoryExchangeRateRepository();
+		service = new ExchangeRateService(repository);
+	}
+
 	@DisplayName("환율 업데이트 - 100단위 외화 API 응답 시 정규화된 1단위 환율 저장 및 원/외화 상호 변환이 정확해야 한다")
 	@Test
 	void updateRates() {
 		// given
-		ExchangeRateRepository repository = new InMemoryExchangeRateRepository();
-		ExchangeRateService service = new ExchangeRateService(repository);
 		// 이벤트 캡처 위해서 ArgumentCaptor 사용
 		ExchangeRateUpdateHandler handler = new ExchangeRateUpdateHandler(service);
 		// 외화 1단위당 원화 몇원
@@ -52,32 +55,10 @@ class ExchangeRateUpdateHandlerTest {
 		Assertions.assertThat(wonResult).isEqualTo(new BigDecimal("999.96"));
 	}
 
-	@DisplayName("환율 업데이트 - DB 저장이 실패하면 캐시도 저장되지 않아야 한다")
-	@Test
-	void updateRates_whenFailSaveRate_thenNotSaveCache() {
-		// given
-		ExchangeRateRepository repository = Mockito.mock(ExchangeRateRepository.class);
-		ExchangeRateService service = new ExchangeRateService(repository);
-		// 환율 저장 무조건 실패
-		BDDMockito.willThrow(RuntimeException.class)
-			.given(repository)
-			.save(ArgumentMatchers.any(ExchangeRate.class));
-		ExchangeRateUpdateHandler handler = new ExchangeRateUpdateHandler(service);
-		ExchangeJsonResponse response = new ExchangeJsonResponse(1, "JPY(100)", "9.1234", "일본 엔화");
-		// when
-		Throwable throwable = Assertions.catchThrowable(() -> handler.handleUpdateRates(response));
-
-		// then
-		Assertions.assertThat(throwable)
-			.isInstanceOf(RuntimeException.class);
-	}
-
 	@DisplayName("환율 업데이트 - DB 저장은 성공했지만, 캐시 업데이트에 실패하는 경우")
 	@Test
 	void updateRates_whenSuccessDBAndFailCacheWork() {
 		// given
-		ExchangeRateRepository repository = new InMemoryExchangeRateRepository();
-		ExchangeRateService service = new ExchangeRateService(repository);
 		// 이벤트 캡처 위해서 ArgumentCaptor 사용
 		ExchangeRateUpdateHandler handler = new ExchangeRateUpdateHandler(service);
 		// 외화 1단위당 원화 몇원
